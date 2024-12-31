27e4672e1dfb55850b975ad981a3f7429cdb6b73
LauraCoelho00 <lauralemoscoelho1@gmail.com>
2024-12-31 00:02:49 +0000

    First Commit

---
+++ src/git_2024041/Git_2024041.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024041;
+
+/**
+ *
+ * @author laura
+ */
+public class Git_2024041 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Laura Lemos Coelho, Student Number: 2024041");
+    }
+    
+}
